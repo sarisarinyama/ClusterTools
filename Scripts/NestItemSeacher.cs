0b1f1d207ec6e95ca07d1b00d21496ad1750f853
Yosuke Kichiraku <yosukekichiraku@gmail.com>
2020-08-03 21:33:48 +0900

    Support for objects outside the scene (Prefab)

--- Scripts/NestItemSeacher.cs
+++ Scripts/NestItemSeacher.cs
@@ -9,8 +9,9 @@ namespace sarisarinyama.cluster
 {
     public class NestItemSeacher : MonoBehaviour
     {
-        [SerializeField] private bool outputItemID;
-        [SerializeField] private bool outputItemName;
+        [SerializeField] private bool outputItemID=true;
+        [SerializeField] private bool outputItemName=true;
+        [SerializeField] private bool searchPrefab=true;
 
         private List<string> parentAndChild = new List<string>();
 
@@ -21,8 +22,9 @@ namespace sarisarinyama.cluster
                 var item = (Item) o;
                 // アセットからパスを取得.シーン上に存在するオブジェクトの場合,シーンファイル（.unity）のパスを取得.
                 string path = AssetDatabase.GetAssetOrScenePath(item.gameObject);
+                bool isScene = true;
                 // シーン上に存在するオブジェクトかどうか文字列で判定.
-                bool isScene = path.Contains(".unity");
+                if(!searchPrefab)isScene= path.Contains(".unity");
                 // シーン上に存在するオブジェクトならば処理.
                 if (isScene)
                 {
